755214c92c0bb24ad678abf5de40c30814b4361e
Zedan Tan <tanzedan@users.noreply.github.com>
2015-06-09 13:35:07 +0800

    Update getprice.m

--- getprice.m
+++ getprice.m
@@ -1,4 +1,4 @@
 LoadFTDIActiveX
-FT_iFinDLogin(iFinDActive,'account','password')
+FT_iFinDLogin(iFinDActive,'account','password') %change your account and password here.
 FTQuerySyn_RealtimeQuotes(iFinDActive,'601918.SH','new;volume','pricetype:1')
 stRTData.tables{1}.table.new{1}
