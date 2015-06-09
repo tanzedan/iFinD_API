2c04c651b5dc03d7176e031b392528014e6b83f2
Zedan Tan <tanzedan@gmail.com>
2015-06-09 13:35:29 +0800

    add notes
    some notes

--- getprice.m
+++ getprice.m
@@ -1,4 +1,10 @@
+% 本示例程序主要用于教程：matlab接口如何调用
+
+% 载入插件
 LoadFTDIActiveX
-FT_iFinDLogin(iFinDActive,'account','password')
+% 登录
+FT_iFinDLogin(iFinDActive,'account','password') %change your account and password here.
+% 获取数据，信息自动保存在stRTData中
 FTQuerySyn_RealtimeQuotes(iFinDActive,'601918.SH','new;volume','pricetype:1')
+% stRTData是一个结构体，找到结构体中的最新价即可展示。
 stRTData.tables{1}.table.new{1}
