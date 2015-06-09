bd71df72451eefc71f9b8c6668b54eb0875a7af6
Zedan Tan <tanzedan@gmail.com>
2015-06-09 13:41:56 +0800

    change
    change

--- getprice.m
+++ getprice.m
@@ -9,8 +9,4 @@ FT_iFinDLogin(iFinDActive,'account','password') %change your account and passwor
 FTQuerySyn_RealtimeQuotes(iFinDActive,'601918.SH','new;volume','pricetype:1')
 % stRTData是一个结构体，找到结构体中的最新价即可展示。
 stRTData.tables{1}.table.new{1}
-=======
-FT_iFinDLogin(iFinDActive,'account','password') %change your account and password here.
-FTQuerySyn_RealtimeQuotes(iFinDActive,'601918.SH','new;volume','pricetype:1')
-stRTData.tables{1}.table.new{1}
->>>>>>> origin/master
+
